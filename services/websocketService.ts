f416033095ffd5b5c9bf517a892c92d765f56f1b
Emily Dawson <lemilydawson@gmail.com>
2024-08-26 11:38:53 +0100

    [feature/05] add connection limit handling to avoid overloading the server

--- services/websocketService.ts
+++ services/websocketService.ts
@@ -2,10 +2,19 @@ import { Server } from 'http'
 import { WebSocketServer } from 'ws'
 import { getCachedStats } from './cacheService'
 
+const maxConnections = 5
+let currentConnections = 0
+
 export const setupWebSocket = (server: Server) => {
 	const wsServer = new WebSocketServer({ server })
 
 	wsServer.on('connection', async (connection) => {
+		if (currentConnections >= maxConnections) {
+			connection.close(1013, 'Server overload')
+			return
+		}
+
+		currentConnections++
 		const data = await getCachedStats()
 		connection.send(JSON.stringify(data))
 
@@ -17,6 +26,7 @@ export const setupWebSocket = (server: Server) => {
 		connection.on('close', () => {
 			console.log('Client disconnected')
 			clearInterval(intervalId)
+			currentConnections--
 		})
 	})
 }
